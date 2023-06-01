1835058612721b4fb45cd5862e7236f14d5bcbdd
NikhilNaik <111115551+NikhilNaik21@users.noreply.github.com>
2023-06-01 23:19:26 +0530

    Create PatternExample.java

---
+++ PatternExample.java
@@ -0,0 +1,31 @@
+package day2;
+
+public class PatternsExample {
+
+	public static void main(String[] args) 
+	{
+		/*
+		for(int i=1;i<5;i++)
+		{
+			for(int j=1;j<5;j++)
+				System.out.print("#");
+			System.out.println();
+		}
+			*/
+		for(int i=0;i<4;i++)
+		{
+			for(int j=0;j<4;j++)
+				//if(i%2==0)
+					System.out.print("#");
+				//else
+				//	System.out.print("*");
+			System.out.println();
+		}
+	
+	
+	
+	}
+
+	
+	
+}
