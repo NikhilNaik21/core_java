012422127b684be9c3094c4dde1568086f1fa276
NikhilNaik <111115551+NikhilNaik21@users.noreply.github.com>
2023-06-02 19:02:25 +0530

    Create trialcodes.java

---
+++ javabyMadhuraMa'm/Day1_Day2/trialcodes.java
@@ -0,0 +1,16 @@
+package Day1_java;
+ 
+public class trialcodes {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int $data=100;
+		int x=  Integer.MAX_VALUE;
+		int y=  Integer.MIN_VALUE;
+		System.out.println($data);
+
+		System.out.println(x);
+		System.out.println(y);
+	}
+
+}
