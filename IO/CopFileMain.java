53174c787b180d7a6b9edd0f813f8dc0d39b8d38
NikhilNaik <111115551+NikhilNaik21@users.noreply.github.com>
2023-07-27 21:26:50 +0530

    Create CopFileMain.java

---
+++ IO/CopFileMain.java
@@ -0,0 +1,27 @@
+package com.iopackage.q1;
+
+import java.util.Date;
+import java.util.Scanner;
+
+public class CopFileMain {
+
+	public static void main(String[] args) {
+		// Write a Program to Copy a File which has the syntax  from source to destination
+		
+		Scanner sc = new Scanner(System.in);
+
+		System.out.println("Enter the source file : ");
+		String source = sc.nextLine();
+		
+		System.out.println("Enter the destination file : ");
+		String dest = sc.nextLine();
+		
+		CopyFileFromSrcToDest obj = new CopyFileFromSrcToDest();
+		Date d = new Date();
+		System.out.println("File copy started at : " + d);
+		obj.CopyFileFromSrcToDest(source,dest);
+		d = new Date();
+		System.out.println("File copy ended at : " + d);
+		
+	}
+}
